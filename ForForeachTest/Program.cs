9b720bffc4f6e0e762a147af1e1132713ce889fe
Nikita Smelov <Nikita_Smelov@epam.com>
2017-10-05 12:06:41 +0300

    init

---
+++ ForForeachTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace ForForeachTest
+{
+    class Program
+    {
+        const int Size = 10000;
+        const int Iterations = 100000;
+
+
+
+        static void Main()
+        {
+            Stopwatch sw;
+            double[] arr = new double[Size];
+            double sum = 0, sum1 = 0;
+            Random rng = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rng.NextDouble();
+            }
+            List<double> list = new List<double>(arr);
+
+            Action<string, Action> runTest = (str, func) =>
+            {
+                sum = 0; sum1 = 0;
+                sw = Stopwatch.StartNew();
+                for (int i = 0; i < Iterations; i++)
+                {
+                    func();
+                }
+                sw.Stop();
+                Console.WriteLine($"Resuls: {sum} {sum1}");
+                Console.WriteLine(str, sw.ElapsedMilliseconds);
+                GC.Collect();
+            };
+
+            runTest("ARR For loop: {0}", () =>
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    sum += arr[j];
+                    sum1 += arr[j] * arr[j];
+                }
+            });
+
+            runTest("ARR Foreach loop: {0}", () =>
+            {
+                foreach (double d in arr)
+                {
+                    sum += d;
+                    sum1 += d * d;
+                }
+            });
+
+            runTest("List For loop: {0}", () =>
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    sum += list[j];
+                    sum1 += list[j] * list[j];
+                }
+            });
+
+            runTest("List Foreach loop: {0}", () =>
+            {
+                foreach (double d in list)
+                {
+                    sum += d;
+                    sum1 += d * d;
+                }
+            });            
+        }
+    }
+}
